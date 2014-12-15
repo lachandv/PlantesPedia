b5270876010576ea56f5c3cff7c0a8b1132017da
valentin <valentin@lachand.net>
2014-12-15 16:46:00 +0100

    Ajout des requêtes

--- PlantesPedia/src/model/OpenDBpedia.java
+++ PlantesPedia/src/model/OpenDBpedia.java
@@ -11,13 +11,59 @@ import org.openrdf.repository.RepositoryException;
 import org.openrdf.repository.http.HTTPRepository;
 
 public class OpenDBpedia {
+    
+    public static String getRequete(String nom, String region, String dpt, String postCode, String nb_hab_min, String nb_hab_max, String sup_min, String sup_max){
+    
+    String req = "Select distinct ?ville Where{ ?ville <http://dbpedia.org/ontology/country> <http://dbpedia.org/resource/France>. ?ville rdf:type <http://fr.dbpedia.org/ontology/Settlement>";
+    
+    int hab_min, hab_max, super_min, super_max;
+    if (region != null) req=req+Region(region);
+    if (dpt != null) req=req+Departement(dpt);
+    if (postCode != null) req=req+Code_postal(Integer.parseInt(postCode));
+    if (nb_hab_min == null) hab_min = 0;
+    else hab_min = Integer.parseInt(nb_hab_min);
+    if (nb_hab_max == null) hab_max = Integer.MAX_VALUE;
+    else hab_max = Integer.parseInt(nb_hab_max);
+    if (sup_min == null) super_min = 0;
+    else super_min = Integer.parseInt(sup_min);
+    if (sup_max == null) super_max = Integer.MAX_VALUE;
+    else super_max = Integer.parseInt(sup_max);
+    req = req+Population(hab_min, hab_max);
+    req = req+Superficie(super_min, super_max);
+    req = req+"} LIMIT 1000";
+        
+    
+    return req;
+}	
+	
+public static String Region(String region){
+    return ". ?ville <HTTP://fr.dbpedia.org/ontology/region> <HTTP://fr.dbpedia.org/resource/"+region+"/>";
+}
+
+public static String Departement(String departement){
+    return ". ?ville <HTTP://fr.dbpedia.org/ontology/department> <HTTP://fr.dbpedia.org/resource/"+departement+"/>";
+}
+public static String Code_postal(int CP){
+    return ". ?ville <HTTP://fr.dbpedia.org/ontology/postalCode> ? CP . FILTER(?CP ="+Integer.toString(CP)+")";
+}
+
+public static String Population(int min, int max){
+    return ". ?ville <HTTP://fr.dbpedia.org/ontology/populationTotal> ? POP . FILTER(?POP >"+Integer.toString(min)+") .FILTER(?POP <"+
+            Integer.toString(max)+")";
+}
+
+public static String Superficie(int min, int max){
+    return ". ?ville <HTTP://fr.dbpedia.org/ontology/area> ? SUP . FILTER(?SUP >"+Integer.toString(min)+") .FILTER(?SUP <"+
+            Integer.toString(max)+")";
+}
 	
 	public static void main(String[] args) throws RepositoryException,MalformedQueryException , QueryEvaluationException{
 		
 		
 		HTTPRepository repo = new HTTPRepository("http://dbpedia.org/sparql","");
 		RepositoryConnection connection = repo.getConnection();
-		TupleQuery query = connection.prepareTupleQuery(QueryLanguage.SPARQL, "select distinct ?Res where {?Res <http://dbpedia.org/ontology/region> <http://dbpedia.org/resource/Rhône-Alpes>} LIMIT 10");
+                String req = getRequete(null, null, null, null, null, null, null, null);
+		TupleQuery query = connection.prepareTupleQuery(QueryLanguage.SPARQL, req);
 		TupleQueryResult result = query.evaluate();
 		while(result.hasNext()){
 			BindingSet binSet = result.next();
@@ -29,8 +75,5 @@ public class OpenDBpedia {
 		connection.close();
 		
 	}
-	
-	
-	
-
+        
 }
